9f3fed04dfa6104f20e09dd9229e3e54ce8891c5
elbub1979 <ilya.leuh09@gmail.com>
2024-10-30 18:58:13 +0300

    added a link to users model in the migration will create test model

--- db/schema.rb
+++ db/schema.rb
@@ -10,50 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_155857) do
-  create_table "answers", force: :cascade do |t|
-    t.text "body", null: false
-    t.boolean "correct", default: false, null: false
-    t.integer "question_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.text "body", null: false
-    t.integer "test_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_questions_on_test_id"
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
-    t.integer "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_tests_on_author_id"
-    t.index ["category_id"], name: "index_tests_on_category_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "authors"
-  add_foreign_key "tests", "categories"
+ActiveRecord::Schema[7.2].define(version: 0) do
 end
