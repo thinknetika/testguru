d41e97aade226797768ff0cf318408cc953e30fb
elbub1979 <ilya.leuh09@gmail.com>
2024-10-30 12:35:26 +0300

    added an empty line between belongs_to & has_many, remove comment in model question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
+
   has_many :answers
 
-  validates :body, length: { minimum: 20 } # проверка минимального количества символов в вопросе
+  validates :body, length: { minimum: 20 }
 end
