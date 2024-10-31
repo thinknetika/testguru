6377539a0fa71c32596cb0d55e23094966b5f0b2
elbub1979 <ilya.leuh09@gmail.com>
2024-10-31 22:56:24 +0300

    minimalistic syntax edit in user migration

--- db/migrate/20241029155857_create_users.rb
+++ db/migrate/20241029155857_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :nickname, null:false
-      t.string :email, null:false
+      t.string :nickname, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
