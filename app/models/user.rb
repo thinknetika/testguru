0746212fbca64cbf160d2d170742058c8688569b
elbub1979 <ilya.leuh09@gmail.com>
2024-11-01 22:22:22 +0300

    refactoring in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   def tests_by_level(level)
     Test.joins("JOIN test_passages ON test_passages.test_id = tests.id").
       where(tests: { level: level }, test_passages: { user_id: id })
