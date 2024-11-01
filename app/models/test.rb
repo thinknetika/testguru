b6257af621a49b09d6b0838ef6be204cae5e1259
elbub1979 <ilya.leuh09@gmail.com>
2024-11-01 19:20:34 +0300

    refactoring sorted_test_by_category class methods (use a single database query)

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,11 @@ class Test < ApplicationRecord
   enum level: { easy: 0, medium: 1, hard: 2 }
 
   def self.sorted_test_by_category(category_title)
-    category_id = Category.find_by(title: category_title).id
-
-    where(category_id: category_id).order(title: :desc).pluck(:title)
+    joins("JOIN categories ON categories.id = tests.category_id").
+      # joins("JOIN categories ON categories.id = tests.category_id AND categories.title = ?", category_title).
+      # where("categories.title = ?", category_title).
+      where(categories: { title: category_title }).
+      order(title: :desc).
+      pluck(:title)
   end
 end
