438c16de6c4971598c8992587fb0a7380dd30054
elbub1979 <ilya.leuh09@gmail.com>
2024-11-02 22:37:18 +0300

    refactoring tests_by_level instance method test model using association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
 
   def tests_by_level(level)
-    Test.joins("JOIN test_passages ON test_passages.test_id = tests.id").
-      where(tests: { level: level }, test_passages: { user_id: id })
+    tests.where(level: level)
   end
 end
