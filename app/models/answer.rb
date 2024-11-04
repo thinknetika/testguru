6adc5816d8e82d12dc59872f0ae76e1fa5ba6445
elbub1979 <ilya.leuh09@gmail.com>
2024-11-04 17:41:01 +0300

    add validates by answer count in answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,15 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :answers_count, on: [ :create, :update ]
 
   scope :correct_answers, -> { where correct: true }
+
+  private
+
+  def answers_count
+    errors.add(
+      :answers_count_error,
+      "there can be no more than four answers") if question.answers.count >= 4
+  end
 end
