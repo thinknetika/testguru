1c53e1e1ede06b554161c5a81356594da0402910
elbub1979 <ilya.leuh09@gmail.com>
2024-11-06 23:32:35 +0300

    add actions new, create, update, destroy in test controller; add _test, _form partials, index, new, show views in test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,12 +1,45 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: [ :show ]
+  before_action :set_test, only: [ :show, :edit, :update, :destroy ]
   def index
     @tests = Test.all
   end
 
   def show; end
 
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to test_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to test_path(@test)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to tests_path
+  end
+
   private
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
 
   def set_test
     @test = Test.find(params[:id])
