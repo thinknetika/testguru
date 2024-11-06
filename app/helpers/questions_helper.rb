e2298240665986e5cd5a46c60130dc0f8be7bede
elbub1979 <ilya.leuh09@gmail.com>
2024-11-07 00:47:11 +0300

    add question header; some refactoring

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,11 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.persisted?
+      verb = "Edit"
+    else
+      verb = "Create"
+    end
+
+    "#{verb} #{question.test.title} question"
+  end
 end
