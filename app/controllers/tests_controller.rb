930ed4956647eb14f25884497644f23f61dfb383
elbub1979 <ilya.leuh09@gmail.com>
2024-11-06 19:31:30 +0300

    add tests views, refactoring tests controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,14 @@
 class TestsController < ApplicationController
+  before_action :set_test, only: [ :show ]
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,14 @@
 module TestsHelper
+  def test_level(test)
+    level = test.level.to_i
+
+    case level
+    when (0..1).include?(level)
+      :easy
+    when (2..4).include?(level)
+      :medium.include?(level)
+    when (5..Float::INFINITY).include?(level)
+      :hard
+    end
+  end
 end
