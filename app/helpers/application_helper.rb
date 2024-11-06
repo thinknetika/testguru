8e2059e9e05bcd5bb8eb34ddd63aece44224fc5b
elbub1979 <ilya.leuh09@gmail.com>
2024-11-07 00:32:20 +0300

    add footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_path
+    link_to "Github", "https://github.com/thinknetika/", target: "_blank"
+  end
+
+  def school_path
+    link_to "Thinknetika", "https://thinknetica.com//", target: "_blank"
+  end
 end
