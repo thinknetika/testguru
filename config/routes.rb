ca8b1f55cb98745bc12d7e8b2d162110b995f179
elbub1979 <ilya.leuh09@gmail.com>
2024-11-07 23:27:55 +0300

    add answers using scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root "tests#index"
+
   resources :tests do
-    resources :questions, except: :index, shallow: true
+    resources :questions, except: :index, shallow: true do
+      resources :answers, except: :index, shallow: true
+    end
   end
 end
