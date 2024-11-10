621440d6b23d2aaabbc6880003505c9b54807eae
elbub1979 <ilya.leuh09@gmail.com>
2024-11-10 21:34:02 +0300

    add migration to add password_digest to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_10_124316) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_10_183255) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
