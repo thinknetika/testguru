ed4363fec0c5573ff6dd1b60d0626bbd42ea3cfb
elbub1979 <ilya.leuh09@gmail.com>
2024-11-10 23:15:44 +0300

    add uniqueness validate to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   has_many :tests, dependent: :destroy
 
-  has_many :test_passages, dependent:  :destroy
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_secure_password
 
