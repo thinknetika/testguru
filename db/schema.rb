54e8ca49f56531f8614bc9212c53b02004893663
elbub1979 <ilya.leuh09@gmail.com>
2024-11-11 23:26:50 +0300

    applied admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_11_194257) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_11_194355) do
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
